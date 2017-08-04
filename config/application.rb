f9eeefe21756471d04373bd339f7347f8a435c27
Paulo Carmino <contato@paulocarmino.com>
2017-08-03 23:21:10 -0300

    Fix logging bug

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Orangechat
   class Application < Rails::Application
-    config.log_level = :debug
-    config.log_tags  = [:subdomain, :uuid]
-    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
